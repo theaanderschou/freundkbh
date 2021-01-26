e9cea5aa7becbb0d9ce54477ab77e7bc8512cfc5
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-01-26 21:09:41 +0100

    burger

--- burger.js
+++ burger.js
@@ -3,7 +3,6 @@ const navslide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-
     burger.addEventListener('click',()=>{
         //Toggle Nav
         nav.classList.toggle('nav-active');
