03b61cc28101b0ea9930cec842cfccb1795e11ff
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-04-21 21:55:54 +0200

    main.js added + burger mediaquery fixed

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+// BURGERMENU
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navslide();
+
+
+
+// COOKIE CONSENT
 const cookieStorage = {
     getItem: (item) => {
         const cookies = document.cookie
@@ -33,3 +63,5 @@ window.onload = () => {
     }
 
 };
+
+
