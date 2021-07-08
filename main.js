6ae79f23e2d38748ed62bd40ea781e2bf5adee92
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-07-08 23:13:56 +0200

    burger 100% resp

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const navslide = () => {
             if (link.style.animation) {
                 link.style.animation = ''
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
             }
         });
 
